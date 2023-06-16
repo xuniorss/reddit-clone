d60b0c0216eb359214e14786f16f943059d36c01
xuniorss <fortunatojunior2020@gmail.com>
2023-06-15 23:22:44 -0300

    initial config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,16 @@
+import { ReactNode } from 'react'
+
 import '@/styles/globals.css'
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
+   title: 'Breadit',
+   description: 'A Reddit clone built with Next.js and TypeScript.',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang='en'>
-      <body>{children}</body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: ReactNode }) {
+   return (
+      <html lang="pt-BR">
+         <body>{children}</body>
+      </html>
+   )
 }
