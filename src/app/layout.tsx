2d89c09ae25e124e8ea4ec8db4cb74a4119e2a47
xuniorss <fortunatojunior2020@gmail.com>
2023-06-16 16:44:30 -0300

    Building the Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,6 @@
+import { Navbar } from '@/components/Navbar'
+import { cn } from '@/lib/utils'
+import { Inter } from 'next/font/google'
 import { ReactNode } from 'react'
 
 import '@/styles/globals.css'
@@ -7,10 +10,20 @@ export const metadata = {
    description: 'A Reddit clone built with Next.js and TypeScript.',
 }
 
+const inter = Inter({ subsets: ['latin'] })
+
 export default function RootLayout({ children }: { children: ReactNode }) {
    return (
-      <html lang="pt-BR">
-         <body>{children}</body>
+      <html
+         lang="pt-BR"
+         className={cn('bg-white text-slate-900 antialiased', inter.className)}
+      >
+         <body className="min-h-screen bg-slate-50 pt-12 antialiased">
+            <Navbar />
+            <div className="container mx-auto h-full max-w-7xl pt-12">
+               {children}
+            </div>
+         </body>
       </html>
    )
 }
