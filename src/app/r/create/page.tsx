1bb764f5cf3e9f00ab9ddc6bd164251e603919fc
xuniorss <fortunatojunior2020@gmail.com>
2023-06-17 19:01:01 -0300

    adjustment

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -2,13 +2,13 @@
 
 import { Button } from '@/components/ui/Button'
 import { Input } from '@/components/ui/Input'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-import { useMutation } from '@tanstack/react-query'
-import axios, { AxiosError } from 'axios'
-import { CreateSubredditPayload } from '@/lib/validators/subreddit'
 import { toast } from '@/hooks/use-toast'
 import { useCustomToast } from '@/hooks/useCustomToast'
+import { CreateSubredditPayload } from '@/lib/validators/subreddit'
+import { useMutation } from '@tanstack/react-query'
+import axios, { AxiosError } from 'axios'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 export default function Create() {
    const [input, setInput] = useState<string>('')
